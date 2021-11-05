bba90823e147897fd5fc5c598bcda909c9e500c6
Ivan Nesterov <nesvanya@rambler.ru>
2021-11-05 22:09:58 +0300

    1

--- Electronic household book/LPH.Designer.cs
+++ Electronic household book/LPH.Designer.cs
@@ -29,12 +29,34 @@ namespace Electronic_household_book
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label_title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label_title
+            // 
+            this.label_title.AutoSize = true;
+            this.label_title.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_title.Location = new System.Drawing.Point(59, 23);
+            this.label_title.Name = "label_title";
+            this.label_title.Size = new System.Drawing.Size(405, 37);
+            this.label_title.TabIndex = 0;
+            this.label_title.Text = "Личное подсобное хозяйство";
+            // 
+            // LPH
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(520, 426);
+            this.Controls.Add(this.label_title);
+            this.Name = "LPH";
             this.Text = "LPH";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_title;
     }
 }
